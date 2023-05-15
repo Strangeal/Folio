72c763464cde60fac8460181a6ed1f228d609c23
strangeal <bajerijustice@gmail.com>
2023-05-15 13:17:09 +0000

    Add experience component

---
+++ src/pages/Experience.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Experience = (props: Props) => {
+  return (
+    <section>
+      <h2>Experience</h2>
+    </section>
+  );
+};
+
+export default Experience;

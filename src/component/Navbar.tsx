3634ea3a39b9b0f5c1f28951189adeb4fa269b2f
strangeal <bajerijustice@gmail.com>
2023-05-17 10:46:32 +0000

    Update navbar component and add styles

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,7 +1,40 @@
-type Props = {};
+import { Avatar, IconButton } from "@mui/material";
+import styles from "../styles/Navbar.module.css";
+import { RiMoonClearFill, RiMoonClearLine } from "react-icons/ri";
+import { useThemeContext } from "./theme/ThemeContextProvider";
+import { Link } from "react-router-dom";
 
-const Navbar = (props: Props) => {
-  return <header>Navbar</header>;
+const Navbar = () => {
+  const { mode, toggleColorMode } = useThemeContext();
+  return (
+    <header className={styles.nav}>
+      <Link to="/">
+        <Avatar
+          alt="developer profile"
+          src="https://res.cloudinary.com/dxsom7jmx/image/upload/v1679052423/photo_2023-03-17_04.24.56_kjjtgy.jpg"
+          sx={{ width: 34, height: 34, borderRadius: 18 }}
+        />
+      </Link>
+      <div className={styles.nav_items}>
+        <Link className={styles.nav_link} to="/about">
+          About
+        </Link>
+        <Link className={styles.nav_link} to="/projects">
+          Projects
+        </Link>
+        <Link className={styles.nav_link} to="/dictionary">
+          Dictionary
+        </Link>
+      </div>
+      <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+        {mode === "dark" ? (
+          <RiMoonClearLine className={styles.theme_icon} />
+        ) : (
+          <RiMoonClearFill />
+        )}
+      </IconButton>
+    </header>
+  );
 };
 
 export default Navbar;
